032a21c1f75205b176713545773df5be0f1a5b78
anajmedd <ayoubnajmeddine99@gmail.com>
2021-12-11 18:34:03 +0100

    Commit config

---
+++ Docker/Routeur_SiteS/Routeur_SiteS.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+ip addr flush dev eth0 
+ip addr flush dev eth1
+
+# Set address on eth0
+ip link set dev eth0 up
+ip addr add 120.0.33.129/26 dev eth0
+
+# Set address on eth1
+ip link set dev eth1 up
+ip addr add 120.0.33.3/24 dev eth1
+
+# Routage statique
+#echo 1 > /proc/sys/net/ipv4/ip_forward
+ip route add 120.0.32.0/20 via 120.0.33.2/24
+ip route add 120.0.33.64/26 via 120.0.33.1/24
+
+
+
