17438c43ae1ab89ce53443772f2976fe6dbe09b7
anajmedd <ayoubnajmeddine99@gmail.com>
2021-12-12 02:48:41 +0100

    routeur entreprise

---
+++ Docker/Routeur_Entreprises/Routeur_Entreprise.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+ip addr flush dev eth0
+ip addr flush dev eth1
+
+# Set address on eth0
+ip link set dev eth0 up
+ip addr add 120.0.32.1/20 dev eth0
+
+# Set address on eth1
+ip link set dev eth1 up
+ip addr add 120.0.33.2/24 dev eth1
+
+# Static routing 
+ip route add 120.0.34.0/24 via 120.0.32.2
+ip route add 120.0.33.128/26 via 120.0.33.3
+ip route add 120.0.33.64/26 via 120.0.33.1
+
+#echo 1 > /proc/sys/net/ipv4/ip_forward
