b547189d0d804d402670255fe295a15dd7766fd2
anajmedd <ayoubnajmeddine99@gmail.com>
2021-12-14 21:26:11 +0100

    commit

--- Docker/Routeur_services/Routeur_services.sh
+++ Docker/Routeur_services/Routeur_services.sh
@@ -11,7 +11,7 @@ ip link set dev eth1 up
 ip addr add 1.1.1.1/24 dev eth1
 
 # Routage statique
-#echo 1 > /proc/sys/net/ipv4/ip_forward
+echo 1 > /proc/sys/net/ipv4/ip_forward
 ip route add 120.0.34.0/24 via 120.0.32.2
 ip route add 120.0.33.0/24 via 120.0.32.1
 ip route add 120.0.48.0/20 via 120.0.32.3
