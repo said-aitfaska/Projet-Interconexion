ddc15591bddd902e9cc2adcf3c5869a1d3b1d410
anajmedd <ayoubnajmeddine99@gmail.com>
2021-12-14 21:22:39 +0100

    commit

--- Docker/Routeur_Entreprises/Routeur_Entreprises.sh
+++ Docker/Routeur_Entreprises/Routeur_Entreprises.sh
@@ -11,8 +11,8 @@ ip link set dev eth1 up
 ip addr add 120.0.33.2/24 dev eth1
 
 # Static routing 
+echo 1 > /proc/sys/net/ipv4/ip_forward
 ip route add 120.0.34.0/24 via 120.0.32.2
 ip route add 120.0.33.128/26 via 120.0.33.3
 ip route add 120.0.33.64/26 via 120.0.33.1
 
-#echo 1 > /proc/sys/net/ipv4/ip_forward
