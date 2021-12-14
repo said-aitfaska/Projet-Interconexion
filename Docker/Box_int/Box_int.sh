c2391baddc8e48a16f6625056a078577b75ed5c6
anajmedd <ayoubnajmeddine99@gmail.com>
2021-12-14 21:21:40 +0100

    commit

--- Docker/Box_int/Box_int.sh
+++ Docker/Box_int/Box_int.sh
@@ -11,6 +11,7 @@ ip link set dev eth1 up
 ip addr add 192.168.1.1/24 dev eth1
 
 # Static routing 
+echo 1 > /proc/sys/net/ipv4/ip_forward
 ip route add 120.0.32.0/20 via 120.0.34.1
 ip route add 192.168.2.0/24 via 120.0.34.3
 
