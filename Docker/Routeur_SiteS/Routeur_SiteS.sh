fa059a937b729fdb4d1d5810b89e94dfbec65c40
anajmedd <ayoubnajmeddine99@gmail.com>
2021-12-14 21:27:50 +0100

    commit

--- Docker/Routeur_SiteS/Routeur_SiteS.sh
+++ Docker/Routeur_SiteS/Routeur_SiteS.sh
@@ -11,7 +11,7 @@ ip link set dev eth1 up
 ip addr add 120.0.33.3/24 dev eth1
 
 # Routage statique
-#echo 1 > /proc/sys/net/ipv4/ip_forward
+echo 1 > /proc/sys/net/ipv4/ip_forward
 ip route add 120.0.32.0/20 via 120.0.33.2/24
 ip route add 120.0.33.64/26 via 120.0.33.1/24
 
