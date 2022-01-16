4c942cb342a8bdac0c97e3546fdec33493f1876b
kingussopp <medmhand1801@gmail.com>
2022-01-16 17:40:21 +0100

    update : :fire: delete routeur_services

--- Docker/Routeur_services/routeur_services.sh
+++
@@ -1,6 +0,0 @@
-ifconfig eth1 0.0.0.0
-ifconfig eth1 120.0.32.4 netmask 255.255.240.0
-ifconfig eth2 0.0.0.0
-ifconfig eth2 1.1.1.1 netmask 255.255.255.0
-route add -net 120.0.33.0/24 gw 120.0.32.1
-route add -net 120.0.34.0/24 gw 120.0.32.2
