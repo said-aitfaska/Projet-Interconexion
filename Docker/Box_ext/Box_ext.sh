67e4730a5238d5af553cbd957408dc055011ed39
anajmedd <ayoubnajmeddine99@gmail.com>
2022-01-22 18:57:36 +0100

    Box_ext

--- Docker/Box_ext/Box_ext.sh
+++ Docker/Box_ext/Box_ext.sh
@@ -1,18 +1,6 @@
-#!/bin/bash
-ip addr flush dev eth0
-ip addr flush dev eth1
-
-# Set address on eth0
-ip link set dev eth0 up
-ip addr add 120.0.34.3/24 dev eth0
-
-# Set address on eth1
-# Adresse a comparer avec l'adresse du reseaux du client de l'AS4
-ip link set dev eth1 up
-ip addr add 192.168.2.1/24 dev eth1
-
-# Static routing
-echo 1 > /proc/sys/net/ipv4/ip_forward 
-ip route add 120.0.32.0/20 via 120.0.34.1
-ip route add 192.168.1.0/24 via 120.0.34.2
-
+ifconfig eth1 0.0.0.0
+ifconfig eth1 120.0.34.3 netmask 255.255.255.0
+ifconfig eth2 0.0.0.0
+ifconfig eth2 192.168.2.1 netmask 255.255.255.0
+route add -net 120.0.32.0/20 gw 120.0.34.1
+route add -net 192.168.1.0/24 gw 120.0.34.3
