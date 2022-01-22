625e875f5af3de84168de3e45f4ffab6522c5455
anajmedd <ayoubnajmeddine99@gmail.com>
2022-01-22 20:09:50 +0100

    ok

--- Docker/Client_int/client_int.sh
+++
@@ -1,9 +0,0 @@
-ifconfig eth1 0.0.0.0
-ifconfig eth1 192.168.1.2 netmask 255.255.255.0
-ifconfig eth2 0.0.0.0
-ifconfig eth2 10.0.0.4 netmask 255.255.255.0
-route add -net 120.0.34.0/24 gw 192.168.1.1
-route add -net 192.168.2.0/24 gw 192.168.1.1
-route add -net 120.0.32.0/20 gw 192.168.1.1
-route add -net 120.0.33.0/24 gw 192.168.1.1
-route add -net 120.0.33.64/26 gw 192.168.1.1
